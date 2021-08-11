3111a11a2792eda8558ac8ff2e9abdfbfe17b290
rohitv <rohitgpverma@gmail.com>
2021-08-11 14:27:02 -0700

    added lombok to User.java and Address.java

--- src/main/java/com/rohit/app/core/model/Address.java
+++ src/main/java/com/rohit/app/core/model/Address.java
@@ -1,11 +1,22 @@
 package com.rohit.app.core.model;
 
-public class Address {
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@ToString
+@EqualsAndHashCode
+public class Address {
+    @Getter @Setter
     private String firstLine;
+    @Getter @Setter
     private String secondLine;
+    @Getter @Setter
     private String city;
+    @Getter @Setter
     private String state;
+    @Getter @Setter
     private int zipCode;
 
 }
--- src/main/java/com/rohit/app/core/model/User.java
+++ src/main/java/com/rohit/app/core/model/User.java
@@ -1,11 +1,23 @@
 package com.rohit.app.core.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@EqualsAndHashCode
+@ToString
 public class User {
 
+    @Getter @Setter
     private String firstName;
+    @Getter @Setter
     private String lastName;
+    @Getter @Setter
     private String preFix;
-
+    @Getter @Setter
+    private ContactInformation contactInformation;
+    @Getter @Setter
     private Address address;
 
 }
